bd4d5887ed79b9d30c5110b24af42e3623c489ef
focusMinor <focus.minor@gmail.com>
2023-01-17 13:52:14 +0500

    17.01.2023 13:52

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -195,6 +195,201 @@ export function setHash(hash) {
    history.pushState("", "", hash);
 }
 
+export let slideUp = (target, duration = 300, showmore = 0) => {
+   if (!target.classList.contains("_slide")) {
+      target.classList.add("_slide");
+      target.style.transitionProperty = "height, margin, padding";
+      target.style.transitionDuration = duration + "ms";
+      target.style.height = `${target.offsetHeight}px`;
+      target.offsetHeight;
+      target.style.overflow = "hidden";
+      target.style.height = showmore ? `${showmore}px` : `0px`;
+      target.style.paddingTop = 0;
+      target.style.paddingBottom = 0;
+      target.style.marginTop = 0;
+      target.style.marginBottom = 0;
+      window.setTimeout(() => {
+         target.hidden = !showmore ? true : false;
+         !showmore ? target.style.removeProperty("height") : null;
+         target.style.removeProperty("padding-top");
+         target.style.removeProperty("padding-bottom");
+         target.style.removeProperty("margin-top");
+         target.style.removeProperty("margin-bottom");
+         !showmore ? target.style.removeProperty("overflow") : null;
+         target.style.removeProperty("transition-duration");
+         target.style.removeProperty("transition-property");
+         target.classList.remove("_slide");
+         // Створюємо подію
+         document.dispatchEvent(
+            new CustomEvent("slideUpDone", {
+               detail: {
+                  target: target,
+               },
+            })
+         );
+      }, duration);
+   }
+};
+
+export let slideDown = (target, duration = 300, showmore = 0) => {
+   if (!target.classList.contains("_slide")) {
+      target.classList.add("_slide");
+      target.hidden = target.hidden ? false : null;
+      showmore ? target.style.removeProperty("height") : null;
+      let height = target.offsetHeight;
+      target.style.overflow = "hidden";
+      target.style.height = showmore ? `${showmore}px` : `0px`;
+      target.style.paddingTop = 0;
+      target.style.paddingBottom = 0;
+      target.style.marginTop = 0;
+      target.style.marginBottom = 0;
+      target.offsetHeight;
+      target.style.transitionProperty = "height, margin, padding";
+      target.style.transitionDuration = duration + "ms";
+      target.style.height = height + "px";
+      target.style.removeProperty("padding-top");
+      target.style.removeProperty("padding-bottom");
+      target.style.removeProperty("margin-top");
+      target.style.removeProperty("margin-bottom");
+      window.setTimeout(() => {
+         target.style.removeProperty("height");
+         target.style.removeProperty("overflow");
+         target.style.removeProperty("transition-duration");
+         target.style.removeProperty("transition-property");
+         target.classList.remove("_slide");
+         // Створюємо подію
+         document.dispatchEvent(
+            new CustomEvent("slideDownDone", {
+               detail: {
+                  target: target,
+               },
+            })
+         );
+      }, duration);
+   }
+};
+
+export let slideToggle = (target, duration = 300) => {
+   if (target.hidden) {
+      return slideDown(target, duration);
+   } else {
+      return slideUp(target, duration);
+   }
+};
+
+export function spollers() {
+   const spollersArray = document.querySelectorAll("[data-spollers]");
+   if (spollersArray.length > 0) {
+      const spollersRegular = Array.from(spollersArray).filter(function (
+         item,
+         index,
+         self
+      ) {
+         return !item.dataset.spollers.split(",")[0];
+      });
+      if (spollersRegular.length) {
+         initSpollers(spollersRegular);
+      }
+      let mdQueriesArray = dataMediaQueries(spollersArray, "spollers");
+      if (mdQueriesArray && mdQueriesArray.length) {
+         mdQueriesArray.forEach((mdQueriesItem) => {
+            mdQueriesItem.matchMedia.addEventListener("change", function () {
+               initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+            });
+            initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
+         });
+      }
+      function initSpollers(spollersArray, matchMedia = false) {
+         spollersArray.forEach((spollersBlock) => {
+            spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
+            if (matchMedia.matches || !matchMedia) {
+               spollersBlock.classList.add("_spoller-init");
+               initSpollerBody(spollersBlock);
+               spollersBlock.addEventListener("click", setSpollerAction);
+            } else {
+               spollersBlock.classList.remove("_spoller-init");
+               initSpollerBody(spollersBlock, false);
+               spollersBlock.removeEventListener("click", setSpollerAction);
+            }
+         });
+      }
+      function initSpollerBody(spollersBlock, hideSpollerBody = true) {
+         let spollerTitles = spollersBlock.querySelectorAll("[data-spoller]");
+         if (spollerTitles.length) {
+            spollerTitles = Array.from(spollerTitles).filter(
+               (item) => item.closest("[data-spollers]") === spollersBlock
+            );
+            spollerTitles.forEach((spollerTitle) => {
+               if (hideSpollerBody) {
+                  spollerTitle.removeAttribute("tabindex");
+                  if (!spollerTitle.classList.contains("_spoller-active")) {
+                     spollerTitle.nextElementSibling.hidden = true;
+                  }
+               } else {
+                  spollerTitle.setAttribute("tabindex", "-1");
+                  spollerTitle.nextElementSibling.hidden = false;
+               }
+            });
+         }
+      }
+      function setSpollerAction(e) {
+         const el = e.target;
+         if (el.closest("[data-spoller]")) {
+            const spollerTitle = el.closest("[data-spoller]");
+            const spollersBlock = spollerTitle.closest("[data-spollers]");
+            const oneSpoller = spollersBlock.hasAttribute("data-one-spoller");
+            const spollerSpeed = spollersBlock.dataset.spollersSpeed
+               ? parseInt(spollersBlock.dataset.spollersSpeed)
+               : 300;
+            if (!spollersBlock.querySelectorAll("._slide").length) {
+               if (
+                  oneSpoller &&
+                  !spollerTitle.classList.contains("_spoller-active")
+               ) {
+                  hideSpollersBody(spollersBlock);
+               }
+               spollerTitle.classList.toggle("_spoller-active");
+               slideToggle(spollerTitle.nextElementSibling, spollerSpeed);
+            }
+            e.preventDefault();
+         }
+      }
+      function hideSpollersBody(spollersBlock) {
+         const spollerActiveTitle = spollersBlock.querySelector(
+            "[data-spoller]._spoller-active"
+         );
+         const spollerSpeed = spollersBlock.dataset.spollersSpeed
+            ? parseInt(spollersBlock.dataset.spollersSpeed)
+            : 300;
+         if (
+            spollerActiveTitle &&
+            !spollersBlock.querySelectorAll("._slide").length
+         ) {
+            spollerActiveTitle.classList.remove("_spoller-active");
+            slideUp(spollerActiveTitle.nextElementSibling, spollerSpeed);
+         }
+      }
+      const spollersClose = document.querySelectorAll("[data-spoller-close]");
+      if (spollersClose.length) {
+         document.addEventListener("click", function (e) {
+            const el = e.target;
+            if (!el.closest("[data-spollers]")) {
+               spollersClose.forEach((spollerClose) => {
+                  const spollersBlock = spollerClose.closest("[data-spollers]");
+                  if (spollersBlock.classList.contains("_spoller-init")) {
+                     const spollerSpeed = spollersBlock.dataset.spollersSpeed
+                        ? parseInt(spollersBlock.dataset.spollersSpeed)
+                        : 300;
+                     spollerClose.classList.remove("_spoller-active");
+                     slideUp(spollerClose.nextElementSibling, spollerSpeed);
+                  }
+               });
+            }
+         });
+      }
+   }
+}
+
 export function tabs() {
    const tabs = document.querySelectorAll("[data-tabs]");
    let tabsActiveHash = [];
@@ -359,210 +554,6 @@ export function tabs() {
    }
 }
 
-export function slideUp(target, duration = 300, showmore = 0) {
-   if (!target.classList.contains("_slide")) {
-      target.classList.add("_slide");
-      target.style.transitionProperty = "height, margin, padding";
-      target.style.transitionDuration = duration + "ms";
-      target.style.height = `${target.offsetHeight}px`;
-      target.offsetHeight;
-      target.style.overflow = "hidden";
-      target.style.height = showmore ? `${showmore}px` : `0px`;
-      target.style.paddingTop = 0;
-      target.style.paddingBottom = 0;
-      target.style.marginTop = 0;
-      target.style.marginBottom = 0;
-      window.setTimeout(() => {
-         target.hidden = !showmore ? true : false;
-         !showmore ? target.style.removeProperty("height") : null;
-         target.style.removeProperty("padding-top");
-         target.style.removeProperty("padding-bottom");
-         target.style.removeProperty("margin-top");
-         target.style.removeProperty("margin-bottom");
-         !showmore ? target.style.removeProperty("overflow") : null;
-         target.style.removeProperty("transition-duration");
-         target.style.removeProperty("transition-property");
-         target.classList.remove("_slide");
-         // Створюємо подію
-         document.dispatchEvent(
-            new CustomEvent("slideUpDone", {
-               detail: {
-                  target: target,
-               },
-            })
-         );
-      }, duration);
-   }
-}
-
-export function slideDown(target, duration = 300, showmore = 0) {
-   if (!target.classList.contains("_slide")) {
-      target.classList.add("_slide");
-      target.hidden = target.hidden ? false : null;
-      showmore ? target.style.removeProperty("height") : null;
-      let height = target.offsetHeight;
-      target.style.overflow = "hidden";
-      target.style.height = showmore ? `${showmore}px` : `0px`;
-      target.style.paddingTop = 0;
-      target.style.paddingBottom = 0;
-      target.style.marginTop = 0;
-      target.style.marginBottom = 0;
-      target.offsetHeight;
-      target.style.transitionProperty = "height, margin, padding";
-      target.style.transitionDuration = duration + "ms";
-      target.style.height = height + "px";
-      target.style.removeProperty("padding-top");
-      target.style.removeProperty("padding-bottom");
-      target.style.removeProperty("margin-top");
-      target.style.removeProperty("margin-bottom");
-      window.setTimeout(() => {
-         target.style.removeProperty("height");
-         target.style.removeProperty("overflow");
-         target.style.removeProperty("transition-duration");
-         target.style.removeProperty("transition-property");
-         target.classList.remove("_slide");
-         // Створюємо подію
-         document.dispatchEvent(
-            new CustomEvent("slideDownDone", {
-               detail: {
-                  target: target,
-               },
-            })
-         );
-      }, duration);
-   }
-}
-
-export function slideToggle(target, duration = 300) {
-   if (target.hidden) {
-      return slideUp(target, duration);
-   } else {
-      return slideDown(target, duration);
-   }
-}
-
-export function spollers() {
-   const spollersArray = document.querySelectorAll("[data-spollers]");
-   if (spollersArray.length > 0) {
-      const spollersRegular = Array.from(spollersArray).filter(function (
-         item,
-         index,
-         self
-      ) {
-         return !item.dataset.spollers.split(",")[0];
-      });
-
-      if (spollersRegular.length) {
-         initSpollers(spollersRegular);
-      }
-
-      let mdQueriesArray = dataMediaQueries(spollersArray, "spollers");
-
-      if (mdQueriesArray && mdQueriesArray.length) {
-         mdQueriesArray.forEach((mdQueriesItem) => {
-            mdQueriesItem.matchMedia.addEventListener("change", function () {
-               initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-            });
-            initSpollers(mdQueriesItem.itemsArray, mdQueriesItem.matchMedia);
-         });
-      }
-
-      function initSpollers(spollersArray, matchMedia = false) {
-         spollersArray.forEach((spollersBlock) => {
-            spollersBlock = matchMedia ? spollersBlock.item : spollersBlock;
-            if (matchMedia.matches || !matchMedia) {
-               spollersBlock.classList.add("_spoller-init");
-               initSpollerBody(spollersBlock);
-               spollersBlock.addEventListener("click", setSpollerAction);
-            } else {
-               spollersBlock.classList.remove("_spoller-init");
-               initSpollerBody(spollersBlock, false);
-               spollersBlock.removeEventListener("click", setSpollerAction);
-            }
-         });
-      }
-
-      function initSpollerBody(spollersBlock, hideSpollerBody = true) {
-         let spollerTitles = spollersBlock.querySelectorAll("[data-spoller]");
-         if (spollerTitles.length) {
-            spollerTitles = Array.from(spollerTitles).filter(
-               (item) => item.closest("[data-spollers]") === spollersBlock
-            );
-            spollerTitles.forEach((spollerTitle) => {
-               if (hideSpollerBody) {
-                  spollerTitle.removeAttribute("tabindex");
-                  if (!spollerTitle.classList.contains("_spoller-active")) {
-                     spollerTitle.nextElementSibling.hidden = true;
-                  }
-               } else {
-                  spollerTitle.setAttribute("tabindex", "-1");
-                  spollerTitle.nextElementSibling.hidden = false;
-               }
-            });
-         }
-      }
-
-      function setSpollerAction(e) {
-         const el = e.target;
-         if (el.closest("[data-spoller]")) {
-            const spollerTitle = el.closest("[data-spoller]");
-            const spollersBlock = spollerTitle.closest("[data-spollers]");
-            const oneSpoller = spollersBlock.hasAttribute("data-one-spoller");
-            const spollerSpeed = spollersBlock.dataset.spollersSpeed
-               ? parseInt(spollersBlock.dataset.spollersSpeed)
-               : 200;
-            if (!spollersBlock.querySelectorAll("._slide").length) {
-               if (
-                  oneSpoller &&
-                  !spollerTitle.classList.contains("_spoller-active")
-               ) {
-                  hideSpollersBody(spollersBlock);
-               }
-               spollerTitle.classList.toggle("_spoller-active");
-               slideToggle(spollerTitle.nextElementSibling, spollerSpeed);
-            }
-            e.preventDefault();
-         }
-      }
-
-      function hideSpollersBody(spollersBlock) {
-         const spollerActiveTitle = spollersBlock.querySelector(
-            "[data-spoller]._spoller-active"
-         );
-         const spollerSpeed = spollersBlock.dataset.spollersSpeed
-            ? parseInt(spollersBlock.dataset.spollersSpeed)
-            : 200;
-         if (
-            spollerActiveTitle &&
-            !spollersBlock.querySelectorAll("._slide").length
-         ) {
-            spollerActiveTitle.classList.remove("_spoller-active");
-            slideUp(spollerActiveTitle.nextElementSibling, spollerSpeed);
-         }
-      }
-
-      const spollersClose = document.querySelectorAll("[data-spoller-close]");
-
-      if (spollersClose.length) {
-         document.addEventListener("click", function (e) {
-            const el = e.target;
-            if (!el.closest("[data-spollers]")) {
-               spollersClose.forEach((spollerClose) => {
-                  const spollersBlock = spollerClose.closest("[data-spollers]");
-                  if (spollersBlock.classList.contains("_spoller-init")) {
-                     const spollerSpeed = spollersBlock.dataset.spollersSpeed
-                        ? parseInt(spollersBlock.dataset.spollersSpeed)
-                        : 200;
-                     spollerClose.classList.remove("_spoller-active");
-                     slideUp(spollerClose.nextElementSibling, spollerSpeed);
-                  }
-               });
-            }
-         });
-      }
-   }
-}
-
 export function bodyLockToggle(delay = 300) {
    if (document.documentElement.classList.contains("lock")) {
       bodyUnlock(delay);
@@ -708,6 +699,12 @@ export function dataMediaQueries(array, dataSetValue) {
          return mdQueriesArray;
       }
    }
+}
+
+export function uniqArray(array) {
+   return array.filter(function (item, index, self) {
+      return self.indexOf(item) === index;
+   });
 }
 
 // ===============================================================================================
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 ```bash
    `
-      ng new --style scss --skip-tests --routing --directory ./
+      ng new --style scss --routing --directory ./
    `
 ```
 
@@ -24,38 +24,4 @@
    `
       "allowJs": true
    `
-```
-
-# svgtofont
-
-```bash
-   `
-      "svgtofont": {
-         "fontName": "icm",
-         "website": false,
-         "outSVGReact": false,
-         "outSVGPath": false,
-         "generateInfoData": false,
-         "css": {
-            "include": "\\.(scss)$",
-            "output": "./src/assets/scss",
-            "fileName": "icons",
-            "cssPath": "../fonts/"
-         }
-      }
-   `
-```
-
-# packages:
-
-```bash
-	`npm i -D svgo svgtofont`
-```
-
-# scripts:
-
-```bash
-	`
-		"svgicons": "svgo -f ./src/assets/svgicons -o ./src/assets/svgicons && svgtofont --sources ./src/assets/svgicons --output ./src/assets/fonts",
-	`
 ```
